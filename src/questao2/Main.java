40abe90a9b5622c67ba766d3892c5e1d068ad2c7
christianagomes <christianaleticia@gmail.com>
2022-06-08 21:12:32 -0300

    teste

---
+++ src/questao2/Main.java
@@ -0,0 +1,7 @@
+package questao2;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/questao3/Main.java
@@ -0,0 +1,7 @@
+package questao3;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
